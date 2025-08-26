bb27b3178cd638e06c5205b7d2b849ece8009bcc
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2025-08-26 10:51:31 +0200

    add a few styles on issue-card

--- src/app/components/issue-card/issue-card.component.ts
+++ src/app/components/issue-card/issue-card.component.ts
@@ -7,8 +7,48 @@ import { CommonModule } from '@angular/common';
   selector: 'app-issue-card',
   imports: [CommonModule, FormsModule],
   templateUrl: './issue-card.component.html',
-  styleUrl: './issue-card.component.css'
+  styleUrl: './issue-card.component.css',
 })
 export class IssueCardComponent {
   @Input() issue!: Issue;
+
+  getCardColor(status: string): string {
+    switch (status) {
+      case 'Open':
+        return 'bg-blue-100';
+      case 'In Progress':
+        return 'bg-orange-100';
+      case 'Closed':
+        return 'bg-green-100';
+      default:
+        return 'bg-gray-50';
+    }
+  }
+
+  getStatusSelectColor(status: string): string {
+    switch (status) {
+      case 'Open':
+        return 'bg-blue-100 text-blue-800';
+      case 'In Progress':
+        return 'bg-orange-100 text-orange-800';
+      case 'Closed':
+        return 'bg-green-100 text-green-800';
+      default:
+        return 'bg-gray-100 text-gray-800';
+    }
+  }
+  
+  getPrioritySelectColor(priority: string): string {
+    switch (priority) {
+      case 'Low':
+        return 'bg-gray-100 text-gray-700';
+      case 'Medium':
+        return 'bg-yellow-100 text-yellow-800';
+      case 'High':
+        return 'bg-red-100 text-red-800';
+      default:
+        return 'bg-gray-100 text-gray-800';
+    }
+  }
+  
 }
